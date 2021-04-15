f17f18635fb7920abb81353ab3f9c606d32ddc70
Linojones <flp_l@hotmail.com>
2021-04-15 20:09:37 -0300

    Classe principal
    Classe principal para execução do programa.

--- src/aplicacao/Principal.java
+++ src/aplicacao/Principal.java
@@ -1,11 +1,19 @@
 package aplicacao;
 
+import modelo.Gerador;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		
+		Gerador gerador = new Gerador();
 		
-
+		gerador.gerarFrase();
+		
+		
+		System.out.println("Frase gerada:");
+		System.out.print(gerador.gerarFrase());
+		
 	}
 
 }
