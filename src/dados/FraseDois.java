1c24ff776652688267e07cf341a30553e0372333
mauriciocoruja <mbjr@outlook.com.br>
2021-10-17 21:37:00 -0300

    refatoração dos atributos e metodos para simplificar o codigo

---
+++ src/dados/FraseDois.java
@@ -0,0 +1,165 @@
+package dados;
+
+public class FraseDois {
+
+    public String getC1L0() {
+        return "a execu��o dos pontos do programa";
+    }
+
+    public String getC1L1() {
+        return "a complexidade dos estudos efetuados";
+    }
+
+    public String getC1L2() {
+        return "a cont�nua expans�o de nossa atividade";
+    }
+
+    public String getC1L3() {
+        return "a estrutura atual da organiza��o";
+    }
+
+    public String getC1L4() {
+        return "o novo modelo estrutural aqui preconizado";
+    }
+
+    public String getC1L5() {
+        return "o desenvolvimento cont�nuo de distintas formas de atua��o";
+    }
+
+    public String getC1L6() {
+        return "a constante divulga��o das informa��es";
+    }
+
+    public String getC1L7() {
+        return "a consolida��o das estruturas";
+    }
+
+    public String getC1L8() {
+        return "a consulta aos diversos militantes";
+    }
+
+    public String getC1L9() {
+        return "o in�cio da atividade geral de forma��o de atitudes";
+    }
+
+    public String getC1L10() {
+        return "o desafiador cen�rio globalizado";
+    }
+
+    public String getC1L11() {
+        return "a mobilidade dos capitais internacionais";
+    }
+
+    public String getC1L12() {
+        return "o fen�meno da Internet";
+    }
+
+    public String getC1L13() {
+        return "a hegemonia do ambiente pol�tico";
+    }
+
+    public String getC1L14() {
+        return "a expans�o dos mercados mundiais";
+    }
+
+    public String getC1L15() {
+        return "o aumento do di�logo entre os diferentes setores produtivos";
+    }
+
+    public String getC1L16() {
+        return "a crescente influ�ncia da m�dia";
+    }
+
+    public String getC1L17() {
+        return "a necessidade de renova��o processual";
+    }
+
+    public String getC1L18() {
+        return "a competitividade nas transa��es comerciais";
+    }
+
+    public String getC1L19() {
+        return "o surgimento do com�rcio virtual";
+    }
+
+    public String getC1L20() {
+        return "a revolu��o dos costumes";
+    }
+
+    public String getC1L21() {
+        return "o acompanhamento das prefer�ncias de consumo";
+    }
+
+    public String getC1L22() {
+        return "o comprometimento entre as equipes";
+    }
+
+    public String getC1L23() {
+        return "a determina��o clara de objetivos";
+    }
+
+    public String getC1L24() {
+        return "a ado��o de pol�ticas descentralizadoras";
+    }
+
+    public String getC1L25() {
+        return "a valoriza��o de fatores subjetivos";
+    }
+
+    public String getC1L26() {
+        return "a percep��o das dificuldades";
+    }
+
+    public String getC1L27() {
+        return "o entendimento das metas propostas";
+    }
+
+    public String getC1L28() {
+        return "o consenso sobre a necessidade de qualifica��o";
+    }
+
+    public String getC1L29() {
+        return "o julgamento imparcial das eventualidades";
+    }
+
+    public String getC1L30() {
+        return "a monetariza��o";
+    }
+
+    public String getC1L31() {
+        return "a globaliza��o";
+    }
+
+    public String getC1L32() {
+        return "o compliance";
+    }
+
+    public String getC1L33() {
+        return "a diversidade";
+    }
+
+    public String getC1L34() {
+        return "a sustentabilidade";
+    }
+
+    public String getC1L35() {
+        return "a converg�ncia";
+    }
+
+    public String getC1L36() {
+        return "o foco no core business";
+    }
+
+    public String getC1L37() {
+        return "a inova��o";
+    }
+
+    public String getC1L38() {
+        return "a responsividade";
+    }
+
+    public String getC1L39() {
+        return "a rede social";
+    }
+
+}
